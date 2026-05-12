d500271122be0e480827222b471e207f4dfa9ffc
JoshuaFC1509 <joshuaflores.ctpa@gmail.com>
2026-05-12 10:10:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarCertificaciones(id){
+
+    let elemento = document.getElementById(id);
+
+    if(elemento.style.display === "block"){
+        elemento.style.display = "none";
+    }else{
+        elemento.style.display = "block";
+    }
+
+}
